bdbf609ba0598b9fa24e9c9d2040c0ce212eef00
Norman Salvador Aráuz López <netman.arauz@gmail.com>
2017-11-25 14:39:48 -0600

    Agregando admin lte

--- transiguia/vendor/yiisoft/extensions.php
+++ transiguia/vendor/yiisoft/extensions.php
@@ -121,4 +121,31 @@ return array (
       '@yii2tech/html2pdf' => $vendorDir . '/yii2tech/html2pdf',
     ),
   ),
+  'rmrevin/yii2-fontawesome' => 
+  array (
+    'name' => 'rmrevin/yii2-fontawesome',
+    'version' => '2.17.1.0',
+    'alias' => 
+    array (
+      '@rmrevin/yii/fontawesome' => $vendorDir . '/rmrevin/yii2-fontawesome',
+    ),
+  ),
+  'cebe/yii2-gravatar' => 
+  array (
+    'name' => 'cebe/yii2-gravatar',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@cebe/gravatar' => $vendorDir . '/cebe/yii2-gravatar/cebe/gravatar',
+    ),
+  ),
+  'dmstr/yii2-adminlte-asset' => 
+  array (
+    'name' => 'dmstr/yii2-adminlte-asset',
+    'version' => '2.5.0.0',
+    'alias' => 
+    array (
+      '@dmstr' => $vendorDir . '/dmstr/yii2-adminlte-asset',
+    ),
+  ),
 );
