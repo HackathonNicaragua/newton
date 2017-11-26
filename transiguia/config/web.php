3dd496ce2d44db4cf2a6b6429379ff0d35235b1d
Norman Salvador Aráuz López <netman.arauz@gmail.com>
2017-11-25 19:19:29 -0600

    Quitar modo debug

--- transiguia/config/web.php
+++ transiguia/config/web.php
@@ -89,7 +89,7 @@ $config = [
     ]
 ];
 
-if (YII_ENV_DEV) {
+/*if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
@@ -105,5 +105,5 @@ if (YII_ENV_DEV) {
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
-
+*/
 return $config;
