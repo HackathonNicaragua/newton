25e25f2db72b855df11f9af85fcac4565cce8fdf
ddelagneau <daviddelagneau@yahoo.es>
2017-11-26 11:56:27 -0600

    Pagina de escuela de conduccion

--- transiguia/views/site/interes.php
+++ transiguia/views/site/interes.php
@@ -1,127 +1,36 @@
 <div class="body-content">
-   <div class="panel panel-default">
-     <div class="panel-heading">
-       <h3>Señales de tránsito Informativas</h3>
-     </div>
-     <div class="panel panel-body">
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/informativas/Aeropuerto.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/informativas/Balneario.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Informativas/CruzRoja.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/informativas/Escaleras.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/informativas/ParadaBus.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Informativas/Telefono.jpg" ?>></img>
-       <br/>
-     </div>
-     </div>
-   </div>
 
+    <div class="row">
+        <div class="col-lg-4">
+          <img style="height:250px" class="img img-responsive img-rounded" src="https://www.cuponclub.net/img/wide_big_thumb/Deal/10078.e9fd393f5f3d66da6e317b46a8fb5d14.jpg" ></img>
+            <h2>Escuela de Manejo Génesis</h2>
+            <p>Escuela de Manejo Génesis de Managua, Nicaragua. Teléfonos, direcciones y
+sucursales de Capacitación En Areas Tecnicas en Páginas Amarillas.</p>
 
-   <div class="panel panel-default">
-     <div class="panel-heading">
-       <h3>Señales de tránsito Preventivas</h3>
-     </div>
-     <div class="panel panel-body">
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/Preventiva/CalzadaResbaladiza.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/CaminoSinuoso.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/CurvaComún.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/Preventiva/encrucigada.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Escolares.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Estrechamiento.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/pendiente_descendiente.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/PerfilIrregular.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Rotonda.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Semaforo.jpg" ?>></img>
-       <br/>
-     </div>
-     </div>
-   </div>
+            <p><a class="btn btn-default" href="https://www.google.com.ni/maps/dir/''/''/@12.1317614,-86.2706272,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x8f73fe9c01bfe49f:0x646a74c02e579ba8!2m2!1d-86.2005866!2d12.1317702">Como llegar &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/curso-conduccion.jpg" ?>></img>
+            <h2>Escuela de Manejo González López</h2>
 
-   <div class="panel panel-default">
-     <div class="panel-heading">
-       <h3>Señales de tránsito Reglamentarias</h3>
-     </div>
-     <div class="panel panel-body">
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/CedaElPaso.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/GiroObligatorio.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoAdelantar.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoAvanzar.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoCAmbiarDeCarril.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoCircular.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoEstacionar.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoGirarEnU.jpg" ?>></img>
-       <br/>
-     </div>
-     <div class="col-lg-3">
-       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/Stop.jpg" ?>></img>
-       <br/>
-     </div>
-     </div>
-   </div>
+            <p>Address: Americas 1 managua, De los Semáforos de la Rafaela 20 vrs abajo, Managua 11185
+Hours: Open today · 8AM–3AM
+Phone: 2252 1515</p>
+
+            <p><a class="btn btn-default" href="https://goo.gl/maps/z5VxP7y3sdT2">Como llegar &raquo;</a></p>
+        </div>
+        <div class="col-lg-4">
+          <img style="height:250px" class="img img-responsive img-rounded"src="http://cdn4.clasificados.com/ni/pictures/photos/000/021/459/original_logo-actual_9111.png"></img>
+            <h2>DriveMaster Management School Nicaragua</h2>
+
+            <p>Address: Resid.Paseo del Valle #58, Pista Sub Urb. Memorial Sandino 1c. oeste, 1c.norte, Managua 13018
+              Realiza una serie de practicas que te ayudaran a saber si estas listo para el examen de conduccion, Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur.</p>
+
+            <p><a class="btn btn-default" href="https://goo.gl/maps/TjAJBg7sSUA2">Como llegar &raquo;</a></p>
+        </div>
+    </div>
 
 </div>
