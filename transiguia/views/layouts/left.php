6ae9f444ad04a5fc285197d821bb7587d38d8279
Norman Salvador Aráuz López <netman.arauz@gmail.com>
2017-11-25 22:13:50 -0600

    Edición del menú

--- transiguia/views/layouts/left.php
+++ transiguia/views/layouts/left.php
@@ -8,31 +8,19 @@
                 <img src="<?= $directoryAsset ?>/img/user2-160x160.jpg" class="img-circle" alt="User Image"/>
             </div>
             <div class="pull-left info">
-                <p>Alexander Pierce</p>
 
-                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
 
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-                <input type="text" name="q" class="form-control" placeholder="Search..."/>
-              <span class="input-group-btn">
-                <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-            </div>
-        </form>
         <!-- /.search form -->
 
         <?= dmstr\widgets\Menu::widget(
             [
                 'options' => ['class' => 'sidebar-menu tree', 'data-widget'=> 'tree'],
                 'items' => [
-                    ['label' => 'Menu Yii2', 'options' => ['class' => 'header']],
+                    ['label' => 'Menu', 'options' => ['class' => 'header']],
                     ['label' => 'Inicio', 'icon' => 'home', 'url' => ['site/index']],
-                    ['label' => '¿Conseguiras tu licencia?', 'icon' => 'fa fa-id-card', 'url' => ['site/index']],
+                    ['label' => 'Prueba tus conocimientos', 'icon' => 'fa fa-id-card', 'url' => ['site/index']],
                     ['label' => '¿Quieres saber más?', 'icon' => 'question-circle', 'url' => ['/debug']],
                     ['label' => 'Calculadora de multas', 'icon' => 'calculator', 'url' => ['/debug']],
                     ['label' => 'Te puede interesar', 'icon' => 'hand-peace-o', 'url' => ['/debug']],
