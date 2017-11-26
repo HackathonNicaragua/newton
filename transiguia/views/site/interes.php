8967591324162e47509b61b606ccd15cc27457ca
ddelagneau <daviddelagneau@yahoo.es>
2017-11-26 11:17:54 -0600

    Pagina señales de transito

--- transiguia/views/site/interes.php
+++ transiguia/views/site/interes.php
@@ -1,7 +1,127 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: norman
- * Date: 25/11/2017
- * Time: 22:18
- */
+<div class="body-content">
+   <div class="panel panel-default">
+     <div class="panel-heading">
+       <h3>Señales de tránsito Informativas</h3>
+     </div>
+     <div class="panel panel-body">
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/informativas/Aeropuerto.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/informativas/Balneario.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Informativas/CruzRoja.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/informativas/Escaleras.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/informativas/ParadaBus.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Informativas/Telefono.jpg" ?>></img>
+       <br/>
+     </div>
+     </div>
+   </div>
+
+
+   <div class="panel panel-default">
+     <div class="panel-heading">
+       <h3>Señales de tránsito Preventivas</h3>
+     </div>
+     <div class="panel panel-body">
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/Preventiva/CalzadaResbaladiza.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/CaminoSinuoso.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/CurvaComún.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/Preventiva/encrucigada.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Escolares.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Estrechamiento.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/pendiente_descendiente.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/PerfilIrregular.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Rotonda.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/Preventiva/Semaforo.jpg" ?>></img>
+       <br/>
+     </div>
+     </div>
+   </div>
+
+   <div class="panel panel-default">
+     <div class="panel-heading">
+       <h3>Señales de tránsito Reglamentarias</h3>
+     </div>
+     <div class="panel panel-body">
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/CedaElPaso.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/GiroObligatorio.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoAdelantar.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded" src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoAvanzar.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoCAmbiarDeCarril.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoCircular.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoEstacionar.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/NoGirarEnU.jpg" ?>></img>
+       <br/>
+     </div>
+     <div class="col-lg-3">
+       <img style="height:250px" class="img img-responsive img-rounded"src=<?php echo Yii::$app->homeUrl."./images/reglamentaria/Stop.jpg" ?>></img>
+       <br/>
+     </div>
+     </div>
+   </div>
+
+</div>
